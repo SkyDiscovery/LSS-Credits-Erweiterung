dc1a75d1536f245c2d3c0aea70e9f53502190552
KBOE2 <mail-an-kboe2@web.de>
2017-11-17 17:10:17 +0100

    Initial commit

---
+++ LSS-Creditserweiterung.user.js
@@ -0,0 +1,112 @@
+// ==UserScript==
+// @name         LSS-Creditserweiterung
+// @version      pre-Alpha
+// @description  In einer Drop-Down-List werden verschiedene Extras zu den Credits angezeigt
+// @author       KBOE2
+// @include      https://www.leitstellenspiel.de/*
+// @grant        none
+// ==/UserScript==
+
+var Credits = document.getElementById("navigation_top").outerHTML;
+var Coins = document.getElementById("coins_top").outerHTML;
+var credits = document.getElementById("navigation_top");
+var coins = document.getElementById("coins_top");
+var gesamtcredits = "undefinied";
+var dienstgrad = "undefinied";
+var next_level = "u.C.";
+
+function lightbox_open() {
+    lightboxOpen();
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function mach_tausender() {
+    var zahl = readCookie("gesamtcredits");
+    if (zahl === null){
+        alert("Um die Credits-erweiterung zu aktivieren, bitte gehe auf dein Profil und lade dann die Seite neu");
+    }
+    var i;
+    var j=0;
+    var ergebnis="";
+    i=zahl.length-1;
+    while (i >= 0) {
+        ergebnis=zahl.substr(i,1)+ergebnis;
+        j++;
+        if (j==3) {
+            ergebnis="."+ergebnis;
+            j=0;
+        }
+        i--;
+    }
+    return ergebnis;
+}
+
+credits_gesamt = mach_tausender();
+
+dienstgrad = readCookie("dienstgrad");
+
+var dienstgrade = ["Anwärter(in)","Feuerwehrmann / Feuerwehrfrau","Oberfeuerwehrmann / Oberfeuerwehrfrau","Hauptfeuerwehrmann / Hauptfeuerwehrfrau","Stv. Gruppenführer(in)","Gruppenführer(in)","Stv. Zugführer(in)","Zugführer(in)","Stv. Wehrführer(in)","Wehrführer(in)","Stv. Kreisbrandmeister(in)","Kreisbrandmeister(in)","Stv. Landesbrandmeister(in)","Landesbrandmeister(in)","Ehrenmitglied"];
+var benoetigte_credits = [0,200,10000,100000,1000000,5000000,10000000,20000000,50000000,100000000,200000000,500000000,1000000000,2000000000,5000000000];
+
+function naechster_dienstgrad(dienstgrad){
+    var i = 0;
+    for (var item in dienstgrade) {
+        if (dienstgrade[item] == dienstgrad){
+            var item_2 = parseInt(item)+1;
+            var benoetigt = benoetigte_credits[item_2];
+            return benoetigt;
+        }
+        i += 1;
+    }
+}
+
+function dienstgrad_next(dienstgrad){
+    for (var item in dienstgrade) {
+        if (dienstgrade[item] == dienstgrad){
+            var item_2 = parseInt(item)+1;
+            var naechster_grad = dienstgrade[item_2];
+            return naechster_grad;
+        }
+    }
+}
+
+function tausender(number) {
+	// Info: Die '' sind zwei Hochkommas
+	number = '' + number;
+	if (number.length > 3) {
+		var mod = number.length % 3;
+		var output = (mod > 0 ? (number.substring(0,mod)) : '');
+		for (i=0 ; i < Math.floor(number.length / 3); i++) {
+			if ((mod === 0) && (i === 0)){
+				output += number.substring(mod+ 3 * i, mod + 3 * i + 3);
+			}
+			else
+
+			// hier wird das Trennzeichen festgelegt mit '.'
+			output+= '.' + number.substring(mod + 3 * i, mod + 3 * i + 3);
+		}
+		return (output);
+	}
+	else return number;
+}
+
+var benoetigt = naechster_dienstgrad(dienstgrad);
+var naechster_grad = dienstgrad_next(dienstgrad);
+gesamtcredits = parseInt(readCookie("gesamtcredits"));
+var verbleibend = benoetigt - gesamtcredits;
+next_level = tausender(parseInt(verbleibend));
+
+$(".nav.navbar-nav.navbar-right").append(`<li ><a id="menu_creditsverwaltung"  class="dropdown_toggle href="#" role="button" data-toggle="dropdown" aria-expanded="false"><img src="http://img5.fotos-hochladen.net/uploads/moneyboxfille9ropt2yxs7.png" heigth="25" width="25"><span class="visible-xs">Creditsverwaltung (von KBOE2)</span><b class="caret"></b></a><ul class="dropdown-menu" role="menu" aria-labelledy="menu_Creditsverwaltung><li class="divider" role="presentation"></li><li role="presentation" >` + Credits + `</li><li role="presentation"><a href="/credits/overview" class="lightbox-open" target="blank" onclick="lightbox_open()" >Credits-Übersicht</a></li><li role="presentation">` + Coins + `</li><li role="presentation"><a href="/coins/list" class="lightbox-open" target="blank" onclick="lightbox_open()">Coinsprotokoll</a></li><li class="divider" role="presentation"></li><li role="presentation"><a>Gesamtcredits: ` + credits_gesamt + `</a></li><li role="presentation"><a>Credits zum nächsten Dienstgrad<br>(`+ naechster_grad + `):<br>`+ next_level + `</a></li><li class="divider" role="presentation"></li><li role="presentation"><a href="https://pastebin.com/embed_js/EbaQHH4f" target="blank" class="lightbox-open" onclick="lightbox_open">Quellcode Teil 1 (immer aktuell)</a></li><li role="presentation"><a class="lightbox-open" target="blank" onclick="lightbox_open()" href="https://pastebin.com/embed_js/UvbWMLwm">Quellcode Teil 2 (immer Aktuell)</a></li><li role="presentation"><a class="lightbox-open" onclick="lightbox_open()" href="https://onedrive.live.com/embed?cid=CD5675D43122E945&resid=CD5675D43122E945%211371&authkey=AFwDk5cpE2TuqZk&em=2&wdStartOn=1&wdEmbedCode=0">Anleitung und Tipps</a></li><li class="divider" role="presentation"></li><li role="presentation"><a href="http://kboe-2.de.tl?in=4" target="blank"><img src="http://www.fotos-hochladen.net/uploads/kb29tnz0o1c3qu.jpg" alt="KBOE2"/> Webseite des Entwicklers (KBOE2)</a></li><li role="presentation"><a class="lightbox-open" target="blank" onclick="lightbox_open()" href="/profile/205976">Profil des Entwicklers (KBOE2)</a></li><li role="presentation"><a href="https://docs.google.com/forms/d/e/1FAIpQLSeEBV6pdU1zPJAEAoIVjdOOxoc_vf88vqaDvdLrS0aanPrasw/viewform" target="blank" class="lightbox-open" onclick="lightbox_open">Programmierauftrag/idee senden</a></li><li class="divider" role="presentation"></li><li role="presentation" onclick="send()"><a >u.C.=under Construction<br>(in Bearbeitung)</a></li><li class="divider" role="presentation"></li><li role="presentation"><a href="https://icons8.com/" target="blank">Icon-Pack</a></li></ul></li>`);
+
+credits.parentNode.remove(credits);
+coins.parentNode.remove(coins);
---
+++ LSS-Creditserweiterung_gesamtcredits.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         LSS-Creditserweiterung_gesamtcredits
+// @version      pre-Alpha
+// @description  Gesamtcredits des Spielers werden gespeichert und im Hauptscript verarbeitet (durch Cookies)
+// @author       KBOE2
+// @include      https://www.leitstellenspiel.de/profile/205976
+// @grant none
+// ==/UserScript==
+
+//!!!!
+//Achtung: bitte im Link oben (https://www.leitstellenspiel.de/profile/205976) noch die Zahl in die eigene Spieler-ID ändern (Kann über weiteres Script direkt im Browser angezeigt werden. Oder: Drop-down-Liste mit Profil und so öffnen, rechts-klick auf Benutzername --> Link speichern --> durch link oben ersetzten)
+//!!!
+
+var regex = /\d/gi, result, indices = [];
+var elements = document.getElementsByClassName("page-header");
+for (var i = 0; i < elements.length; i++){
+  var element = elements[i];
+  var ausgabe = element.innerHTML;
+  ausgabe = ausgabe.replace(/ /,"");
+  ausgabe = ausgabe.replace(/\n|\r/g,"");
+  ausgabe = ausgabe.replace(/<div.+div>|\r/g,"");
+  ausgabe = ausgabe.replace(/<h1.+h1>|\r/g,"");
+  ausgabe = ausgabe.replace(/<a.+a>|\r/g,"");
+  ausgabe = ausgabe.replace(/<br>|\r/g,"");
+  ausgabe = ausgabe.replace("Verband:","");
+  ausgabe = ausgabe.replace("Dienstgrad:","");
+  ausgabe = ausgabe.replace("verdiente Credits","");
+  var gesamtcredits = "";
+    while (result = regex.exec(ausgabe)) {
+    indices.push("" + result.index);
+    gesamtcredits+=result;
+  }
+  ausgabe = ausgabe.replace(/\d.+\d/,"");
+  var dienstgrad = ausgabe;
+  document.cookie = "gesamtcredits=" + gesamtcredits + "; path=/";
+  document.cookie = "dienstgrad=" + dienstgrad + "; path=/";
+}
