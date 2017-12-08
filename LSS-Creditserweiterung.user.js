b1809c68129c1c421cda256a3a26852c06c37c13
KBOE2 <mail-an-kboe2@web.de>
2017-12-08 17:30:56 +0100

    Stashed changes, as they didn't worked

--- LSS-Creditserweiterung.user.js
+++ LSS-Creditserweiterung.user.js
@@ -17,10 +17,6 @@ var coins = document.getElementById("coins_top");
 var gesamtcredits = "undefined";
 var dienstgrad = "undefined";
 var next_level = "undefined";
-var credits_gesamt = 0;
-var naechster_grad = "undefined";
-var dienstgrade = ["Anwärter(in)","Feuerwehrmann / Feuerwehrfrau","Oberfeuerwehrmann / Oberfeuerwehrfrau","Hauptfeuerwehrmann / Hauptfeuerwehrfrau","Stv. Gruppenführer(in)","Gruppenführer(in)","Stv. Zugführer(in)","Zugführer(in)","Stv. Wehrführer(in)","Wehrführer(in)","Stv. Kreisbrandmeister(in)","Kreisbrandmeister(in)","Stv. Landesbrandmeister(in)","Landesbrandmeister(in)","Ehrenmitglied"];
-    var benoetigte_credits = [0,200,10000,100000,1000000,5000000,10000000,20000000,50000000,100000000,200000000,500000000,1000000000,2000000000,5000000000];
 
 function lightbox_open() {
     lightboxOpen();
@@ -49,6 +45,13 @@ function mach_tausender() {
     return ergebnis;
 }
 
+credits_gesamt = mach_tausender();
+
+dienstgrad = readCookie("dienstgrad");
+
+var dienstgrade = ["Anwärter(in)","Feuerwehrmann / Feuerwehrfrau","Oberfeuerwehrmann / Oberfeuerwehrfrau","Hauptfeuerwehrmann / Hauptfeuerwehrfrau","Stv. Gruppenführer(in)","Gruppenführer(in)","Stv. Zugführer(in)","Zugführer(in)","Stv. Wehrführer(in)","Wehrführer(in)","Stv. Kreisbrandmeister(in)","Kreisbrandmeister(in)","Stv. Landesbrandmeister(in)","Landesbrandmeister(in)","Ehrenmitglied"];
+var benoetigte_credits = [0,200,10000,100000,1000000,5000000,10000000,20000000,50000000,100000000,200000000,500000000,1000000000,2000000000,5000000000];
+
 function naechster_dienstgrad(dienstgrad){
     var i = 0;
     for (var item in dienstgrade) {
@@ -71,33 +74,19 @@ function dienstgrad_next(dienstgrad){
     }
 }
 
-function gesamtcredits_aktualisieren(){
-    profil = window.open(document.getElementById("navbar_profile_link").href, "profil", "width=1,height=1,status=yes,scrollbars=yes,resizable=yes");
-    window.setTimeout(1000);
-    profil.close();
+var benoetigt = naechster_dienstgrad(dienstgrad);
+var naechster_grad = dienstgrad_next(dienstgrad);
+gesamtcredits = parseInt(readCookie("gesamtcredits"));
+var verbleibend = benoetigt - gesamtcredits;
+next_level = parseInt(verbleibend).toLocaleString();
+if (credits_gesamt == "NaN"){
+	credits_gesamt = "Bitte auf das Profil gehen<br>und dann die Seite neu laden";
+}
+if (next_level == "NaN"){
+	next_level = "Bitte auf das Profil gehen<br>und dann die Seite neu laden";
 }
 
-function aktualisieren(){
-    gesamtcredits_aktualisieren();
-    credits_gesamt = mach_tausender();
-    dienstgrad = readCookie("dienstgrad");
-    var benoetigt = naechster_dienstgrad(dienstgrad);
-    var naechster_grad = dienstgrad_next(dienstgrad);
-    gesamtcredits = parseInt(readCookie("gesamtcredits"));
-    var verbleibend = benoetigt - gesamtcredits;
-    next_level = parseInt(verbleibend).toLocaleString();
-    if (credits_gesamt == "NaN"){
-        credits_gesamt = "Bitte auf das Profil gehen<br>und dann die Seite neu laden";
-    }
-    if (next_level == "NaN"){
-        next_level = "Bitte auf das Profil gehen<br>und dann die Seite neu laden";
-    }
-}
-
-aktualisieren();
-setInterval(aktualisieren, 300000);
-
-$(".nav.navbar-nav.navbar-right").append('<li ><a id="menu_creditsverwaltung"  class="dropdown_toggle href="#" role="button" data-toggle="dropdown" aria-expanded="false"><img src="https://img.webme.com/pic/k/kboe-2/icons8-money-box-50.png" heigth="25" width="25"><span class="visible-xs">Creditsverwaltung (von KBOE2)</span><b class="caret"></b></a><ul class="dropdown-menu" role="menu" aria-labelledy="menu_Creditsverwaltung><li class="divider" role="presentation"></li><li role="presentation" >' + Credits + '</li><li role="presentation"><a href="/credits/overview" class="lightbox-open" target="blank" onclick="lightbox_open()" >Credits-Übersicht</a></li><li role="presentation">' + Coins + '</li><li role="presentation"><a href="/coins/list" class="lightbox-open" target="blank" onclick="lightbox_open()">Coinsprotokoll</a></li><li class="divider" role="presentation"></li><li role="presentation"><a>Gesamtcredits: ' + credits_gesamt + '</a><a>Credits zum nächsten Dienstgrad<br>(' + naechster_grad + '):<br>' + next_level + '</a></li><li class="divider" role="presentation"></li><li role="presentation"><a class="lightbox-open" target="blank" onclick="lightbox_open()" href="/profile/205976">Profil des Entwicklers (KBOE2)</a></li></ul></li>');
+$(".nav.navbar-nav.navbar-right").append('<li ><a id="menu_creditsverwaltung"  class="dropdown_toggle href="#" role="button" data-toggle="dropdown" aria-expanded="false"><img src="https://img.webme.com/pic/k/kboe-2/icons8-money-box-50.png" heigth="25" width="25"><span class="visible-xs">Creditsverwaltung (von KBOE2)</span><b class="caret"></b></a><ul class="dropdown-menu" role="menu" aria-labelledy="menu_Creditsverwaltung><li class="divider" role="presentation"></li><li role="presentation" >' + Credits + '</li><li role="presentation"><a href="/credits/overview" class="lightbox-open" target="blank" onclick="lightbox_open()" >Credits-Übersicht</a></li><li role="presentation">' + Coins + '</li><li role="presentation"><a href="/coins/list" class="lightbox-open" target="blank" onclick="lightbox_open()">Coinsprotokoll</a></li><li class="divider" role="presentation"></li><li role="presentation"><a>Gesamtcredits: ' + credits_gesamt + '</a><a>Credits zum nächsten Dienstgrad<br>(' + naechster_grad + '):<br>'+ next_level + '</a></li><li class="divider" role="presentation"></li><li role="presentation"><a class="lightbox-open" target="blank" onclick="lightbox_open()" href="/profile/205976">Profil des Entwicklers (KBOE2)</a></li></ul></li>');
 
 credits.parentNode.remove(credits);
 coins.parentNode.remove(coins);
